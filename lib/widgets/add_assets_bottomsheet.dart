4a50d4ac97a2e0d59ca295d2e984a07d221d2ea1
Dare Oloruntoba <64743870+dretoba91@users.noreply.github.com>
2024-07-24 02:07:56 +0100

    fix keyboardheight issue inside the bottomsheet

--- lib/widgets/add_assets_bottomsheet.dart
+++ lib/widgets/add_assets_bottomsheet.dart
@@ -5,11 +5,68 @@ import 'package:cryptofolio/helpers/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class AddAssetsBottomsheet extends StatelessWidget {
+class AddAssetsBottomsheet extends StatefulWidget {
   AddAssetsBottomsheet({super.key});
+
+  @override
+  State<AddAssetsBottomsheet> createState() => _AddAssetsBottomsheetState();
+}
+
+class _AddAssetsBottomsheetState extends State<AddAssetsBottomsheet>
+    with WidgetsBindingObserver {
   final addAssetsController = Get.put(AddAssetsController());
+
   final ScrollController _scrollController = ScrollController();
+
   TextEditingController textController = TextEditingController();
+  final _focusNode = FocusNode();
+  double _bottomInset = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _focusNode.addListener(_onFocusChange);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    _focusNode.removeListener(_onFocusChange);
+    _focusNode.dispose();
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _onFocusChange() {
+    if (_focusNode.hasFocus) {
+      _scrollToFocusedInput();
+    }
+  }
+
+  void _scrollToFocusedInput() {
+    Future.delayed(Duration(milliseconds: 300), () {
+      _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    });
+  }
+
+  @override
+  void didChangeMetrics() {
+    final bottomInset = WidgetsBinding.instance.window.viewInsets.bottom;
+    if (bottomInset != _bottomInset) {
+      setState(() {
+        _bottomInset = bottomInset;
+      });
+      if (_focusNode.hasFocus) {
+        _scrollToFocusedInput();
+      }
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -76,13 +133,13 @@ class AddAssetsBottomsheet extends StatelessWidget {
                         ),
                         TextField(
                           controller: textController,
+                          focusNode: _focusNode,
                           style: const TextStyle(color: Colors.white),
                           decoration: const InputDecoration(
-                            hintText: 'Enter some text',
+                            hintText: 'Enter some value',
                             hintStyle: TextStyle(
                               color: Colors.white,
                             ),
-                            
                             border: OutlineInputBorder(),
                           ),
                           keyboardType: TextInputType.number,
@@ -92,15 +149,13 @@ class AddAssetsBottomsheet extends StatelessWidget {
                                 double.parse(value);
 
                             log("inside text field ${textController.text}");
-                            _scrollController.animateTo(
-                              _scrollController.position.maxScrollExtent,
-                              duration: const Duration(milliseconds: 300),
-                              curve: Curves.easeOut,
-                            );
                           },
                           onSubmitted: (value) {
                             // Handle form submission
                           },
+                        ),
+                        SizedBox(
+                          height: 250,
                         ),
                       ],
                     ),
